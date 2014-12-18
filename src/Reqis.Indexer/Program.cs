099b735a0458f13af7763f7d502502f0f787ff1e
a-wall <andrewwall@hotmail.com>
2014-12-18 14:58:08 +1300

    Initial solution and projects.

---
+++ src/Reqis.Indexer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reqis.Indexer
+{
+    public sealed class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
